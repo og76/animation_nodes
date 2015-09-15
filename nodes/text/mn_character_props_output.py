e82e4ecebd672f9d70b9b1eae841df8b90bae586
og76 <o.g.blender@gmail.com>
2015-09-16 02:21:42 +0300

    some cleaning needed
    and an example blend to understand

--- nodes/text/mn_character_props_output.py
+++ nodes/text/mn_character_props_output.py
@@ -4,11 +4,11 @@ from bpy.props import *
 from ... mn_node_base import AnimationNode
 from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
-options = [ ("materialIndex", "Material Index"),
-            ("useBold", "Bold"),
-            ("useItalic", "Italic"),
-            ("useUnderline", "Underline"),
-            ("useSmallCaps", "Small Caps")]
+#options = [ ("materialIndex", "Material Index"),
+#            ("useBold", "Bold"),
+#            ("useItalic", "Italic"),
+#            ("useUnderline", "Underline"),
+#            ("useSmallCaps", "Small Caps")]
 
 class mn_CharacterPropsOutputNode(Node, AnimationNode):
     bl_idname = "mn_CharacterPropsOutputNode"
@@ -18,31 +18,32 @@ class mn_CharacterPropsOutputNode(Node, AnimationNode):
         self.setHideProperty()
         nodeTreeChanged()
     
-    materialIndex = BoolProperty(default = True, update = usePropertyChanged)
+    materialIndex = BoolProperty(default = True, description ="Set Material Index", update = usePropertyChanged)
     
-    useBold = BoolProperty(default = False, update = usePropertyChanged)
-    useItalic = BoolProperty(default = False, update = usePropertyChanged)
-    useUnderline = BoolProperty(default = False, update = usePropertyChanged)
-    useSmallCaps = BoolProperty(default = False, update = usePropertyChanged)
+    useBold = BoolProperty(default = False, description ="Use Bold", update = usePropertyChanged)
+    useItalic = BoolProperty(default = False, description ="Use Italic", update = usePropertyChanged)
+    useUnderline = BoolProperty(default = False, description ="Underline", update = usePropertyChanged)
+    useSmallCaps = BoolProperty(default = False, description ="Small Caps", update = usePropertyChanged)
     
     allowNegativeIndex = BoolProperty(default = True)
     
     def init(self, context):
         forbidCompiling()
+        
         self.inputs.new("mn_ObjectSocket", "Text Object").showName = False
         
-        self.inputs.new("mn_IntSocket", "Start").number = 0.0
-        self.inputs.new("mn_IntSocket", "End").number = -1.0
-        self.inputs.new("mn_IntSocket", "Material Index").number = 0.0
+        self.inputs.new("mn_IntegerSocket", "Start").number = 0
+        self.inputs.new("mn_IntegerSocket", "End").number = -1
+        self.inputs.new("mn_IntegerSocket", "Material Index").number = 0
         
-        self.inputs.new("mn_BoolSocket", "Bold").value = False
-        self.inputs.new("mn_BoolSocket", "Italic").value = False
-        self.inputs.new("mn_BoolSocket", "Underline").value = False
+        self.inputs.new("mn_BooleanSocket", "Bold").value = False
+        self.inputs.new("mn_BooleanSocket", "Italic").value = False
+        self.inputs.new("mn_BooleanSocket", "Underline").value = False
         
         self.inputs.new("mn_FloatSocket", "Underline Position").number = 0.0
         self.inputs.new("mn_FloatSocket", "Underline Thickness").number = 0.05
         
-        self.inputs.new("mn_FloatSocket", "Small Caps").value = False
+        self.inputs.new("mn_BooleanSocket", "Small Caps").value = False
         self.inputs.new("mn_FloatSocket", "Small Caps Scale").number = 0.75
         self.setHideProperty()
         
@@ -57,7 +58,7 @@ class mn_CharacterPropsOutputNode(Node, AnimationNode):
         row.prop(self, "useBold", text = "", icon = "FONT_DATA")
         row.prop(self, "useItalic", text = "", icon = "OUTLINER_DATA_FONT")
         row.prop(self, "useUnderline", text = "", icon = "FONTPREVIEW")
-        row.prop(self, "useSmallCaps", text = "", icon = "FILE_FONT")   #SYNTAX_OFF
+        row.prop(self, "useSmallCaps", text = "", icon = "SYNTAX_OFF")   #FILE_FONT
 
             
     def draw_buttons_ext(self, context, layout):
@@ -65,17 +66,17 @@ class mn_CharacterPropsOutputNode(Node, AnimationNode):
         layout.prop(self, "allowNegativeIndex")
         
     def setHideProperty(self):
-        for option in options:
-            self.inputs["Material Index"].hide = not self.materialIndex
-            self.inputs["Bold"].hide = not self.useBold
-            self.inputs["Italic"].hide = not self.useItalic
+
+        self.inputs["Material Index"].hide = not self.materialIndex
+        self.inputs["Bold"].hide = not self.useBold
+        self.inputs["Italic"].hide = not self.useItalic
             
-            self.inputs["Underline"].hide = not self.useUnderline
-            self.inputs["Underline Position"].hide = not self.useUnderline
-            self.inputs["Underline Thickness"].hide = not self.useUnderline
+        self.inputs["Underline"].hide = not self.useUnderline
+        self.inputs["Underline Position"].hide = not self.useUnderline
+        self.inputs["Underline Thickness"].hide = not self.useUnderline
             
-            self.inputs["Small Caps"].hide = not self.useSmallCaps
-            self.inputs["Small Caps Scale"].hide = not self.useSmallCaps
+        self.inputs["Small Caps"].hide = not self.useSmallCaps
+        self.inputs["Small Caps Scale"].hide = not self.useSmallCaps
             
     def getInputSocketNames(self):
         return {"Text Object" : "object",
@@ -102,22 +103,21 @@ class mn_CharacterPropsOutputNode(Node, AnimationNode):
         codeLines.append("    textObject = None")
         codeLines.append("    if %object%.type == 'FONT': textObject = %object%.data")
         codeLines.append("    if textObject is not None:")
-        if self.useUnderline: codeLines.append(" "*8 + "textObject.underline_position = %underlinePosition%" +
-                                        "\n" + " "*8 + "textObject.underline_height = %underlineThickness%")
+        if self.useUnderline: codeLines.append(" "*8 + "textObject.underline_position = %underlinePosition%" +"\n" + " "*8 + "textObject.underline_height = %underlineThickness%")
         if self.useSmallCaps: codeLines.append(" "*8 + "textObject.small_caps_scale = %smallCapsScale%")
         
-        codeLines.append(" "*8 + "s, e = start, end")
-        if not self.allowNegativeIndex: codeLines.append(" "*8 + "s, e = max(0, start), max(0, end)")
+        if self.allowNegativeIndex: codeLines.append(" "*8 + "s, e = %start%, %end%")
+        else: codeLines.append(" "*8 + "s, e = max(0, %start%), max(0, %end%)")
         
         codeLines.append(" "*8 + "for char in textObject.body_format[s:e]:")
         if self.materialIndex: codeLines.append(" "*12 + "char.material_index = %materialIndex%")
         if self.useBold: codeLines.append(" "*12 + "char.use_bold = %bold%")
         if self.useItalic: codeLines.append(" "*12 + "char.use_italic = %italic%")
-        if self.useUnderline: codeLines.append(" "*12 + "char.use_underline = %underline%") +
+        if self.useUnderline: codeLines.append(" "*12 + "char.use_underline = %underline%")
 
         
         if self.useSmallCaps: codeLines.append(" "*12 + "char.use_small_caps = %smallCaps%")
         
-        codeLines.append(" "*8 + "pass")
+        codeLines.append(" "*12 + "pass")
         
         return "\n".join(codeLines)
