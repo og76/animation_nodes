2e1a3bfc95cc82c12e9ac23d0c11e7cfa65188c5
og76 <o.g.blender@gmail.com>
2016-05-11 11:51:30 +0300

    soc line plane

--- nodes/geometry/intersect_line_plane.py
+++ nodes/geometry/intersect_line_plane.py
@@ -4,17 +4,17 @@ from ... base_types.node import AnimationNode
 class IntersectLinePlaneNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_IntersectLinePlaneNode"
     bl_label = "Intersect Line Plane"
-
+    bl_width_default = 160
+    
     def create(self):
-        self.width = 160
-        self.inputs.new("an_VectorSocket", "Line Start", "lineStart")
-        self.inputs.new("an_VectorSocket", "Line End", "lineEnd").value = (0, 0, 1)
+        self.newInput("Vector", "Line Start", "lineStart")
+        self.newInput("Vector", "Line End", "lineEnd").value = (0, 0, 1)
 
-        self.inputs.new("an_VectorSocket", "Plane Point", "planePoint")
-        self.inputs.new("an_VectorSocket", "Plane Normal", "planeNormal").value = (0, 0, 1)
+        self.newInput("Vector", "Plane Point", "planePoint")
+        self.newInput("Vector", "Plane Normal", "planeNormal").value = (0, 0, 1)
 
-        self.outputs.new("an_VectorSocket", "Intersection", "intersection")
-        self.outputs.new("an_BooleanSocket", "Is Valid", "isValid")
+        self.newOutput("Vector", "Intersection", "intersection")
+        self.newOutput("Boolean", "Is Valid", "isValid")
 
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
