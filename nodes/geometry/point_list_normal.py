778c1e80c3abe0b3fb4e62a860994969983117af
og76 <o.g.blender@gmail.com>
2016-05-11 11:50:44 +0300

    corrections normal

--- nodes/geometry/point_list_normal.py
+++ nodes/geometry/point_list_normal.py
@@ -6,15 +6,16 @@ from ... base_types.node import AnimationNode
 class PointListNormalNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_PointListNormalNode"
     bl_label = "Point List Normal"
+    bl_width_default = 160
     
     searchTags = ["Points Normal", "Calculate Normal"]    
     errorMessage = StringProperty()
         
     def create(self):
         self.width = 160
-        self.inputs.new("an_VectorListSocket", "Point List", "points")
-        self.outputs.new("an_VectorSocket", "Normal", "normal")
-        self.outputs.new("an_BooleanSocket", "Is Valid", "isValid")
+        self.newInput("Vector List", "Point List", "points")
+        self.newOutput("Vector", "Normal", "normal")
+        self.newOutput("Boolean", "Is Valid", "isValid")
         
     def draw(self, layout):
         if self.errorMessage != "":
@@ -24,7 +25,7 @@ class PointListNormalNode(bpy.types.Node, AnimationNode):
         yield "if len(points) > 2: normal, self.errorMessage = mathutils.geometry.normal(points), ''"
         yield "else: normal, self.errorMessage = mathutils.Vector((0, 0, 0)), 'Expected min 3 different vectors'"
 
-        yield "isValid = normal != mathutils.Vector((0, 0, 0))"
+        yield "isValid = normal[:] != (0, 0, 0)"
         
     def getUsedModules(self):
         return ["mathutils"]
