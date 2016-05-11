6a90c6da02a16a197d2c1ebdcd8568c88ac07cae
og76 <o.g.blender@gmail.com>
2016-05-11 11:52:17 +0300

    soc sph pl

--- nodes/geometry/intersect_sphere_plane.py
+++ nodes/geometry/intersect_sphere_plane.py
@@ -4,17 +4,17 @@ from ... base_types.node import AnimationNode
 class IntersectSpherePlaneNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_IntersectSpherePlaneNode"
     bl_label = "Intersect Sphere Plane"
-
+    bl_width_default = 160
+    
     def create(self):
-        self.width = 160
-        self.inputs.new("an_VectorSocket", "Sphere Center", "c")
-        self.inputs.new("an_FloatSocket", "Sphere Radius", "r").value = 1
-        self.inputs.new("an_VectorSocket", "Plane Point", "point")
-        self.inputs.new("an_VectorSocket", "Plane Normal", "normal").value = (0, 0, 1)
+        self.newInput("Vector", "Sphere Center", "c")
+        self.newInput("Float", "Sphere Radius", "r").value = 1
+        self.newInput("Vector", "Plane Point", "point")
+        self.newInput("Vector", "Plane Normal", "normal").value = (0, 0, 1)
         
-        self.outputs.new("an_VectorSocket", "Circle Center", "center")
-        self.outputs.new("an_FloatSocket", "Circle Radius", "radius")
-        self.outputs.new("an_BooleanSocket", "Is Valid", "isValid").hide = True
+        self.newOutput("Vector", "Circle Center", "center")
+        self.newOutput("Float", "Circle Radius", "radius")
+        self.newOutput("Boolean", "Is Valid", "isValid").hide = True
         
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
@@ -23,16 +23,15 @@ class IntersectSpherePlaneNode(bpy.types.Node, AnimationNode):
         center  = isLinked["center"]
         radius  = isLinked["radius"]
         isValid = isLinked["isValid"]
-        zero = "mathutils.Vector((0,0,0))"
         
         yield "dist = mathutils.geometry.distance_point_to_plane(c, point, normal)"
     
-        yield "if abs(dist) <= r and r != 0:"
+        yield "if abs(dist) <= abs(r) and r != 0:"
         if center : yield "    center = (normal * -dist) + c"
-        if radius : yield "    radius = math.sqrt((r**2) - (dist**2))"
+        if radius : yield "    radius = 2 * math.sqrt((r**2) - (dist**2))"
         if isValid: yield "    isValid = True"
         yield "else:"
-        if center : yield "    center =" + zero
+        if center : yield "    center = mathutils.Vector((0,0,0))"
         if radius : yield "    radius = 0"
         if isValid: yield "    isValid = False"
         
